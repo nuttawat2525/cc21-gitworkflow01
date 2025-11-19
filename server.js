2dcfb5585cf69803f297523e7515159271a8e889
Nie-ent <napatns42@gmail.com>
2025-11-19 15:17:14 +0700

    add compression

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import { config } from 'dotenv'
+import compression from 'compression'
 
 config()
 
 const app = express()
 const PORT = process.env.PORT || 8000
+
+app.use(compression())
 
 app.listen(8000, () => {
     console.log(`server is running`)
